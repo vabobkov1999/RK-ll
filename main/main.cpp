1e9b4a5a5a47b191f8cf20a8407cc59f0a3ba30f
Vasily <34238099+MorozzoFFF@users.noreply.github.com>
2018-04-28 00:08:07 +0300

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,42 +1,39 @@
-#include "Queue.hpp"
+#include "queue.hpp"
 #include <sstream>
 #include <fstream>
 
 using namespace std;
 
-int main()  
-{
-    Queue<int> q1;
-    Queue<int> q2 = {1, 2, 3, 4};
-    Queue<int> q3{q2};
-    cout << q2 << endl;
-    cout << q3 << endl;
-    q1.swap(q3);
-    cout << q1 << endl;
-    cout << boolalpha << q3.empty() << endl;
-    cout << q1.size() << endl;
-    cout << q2.size() << endl;
-    q3.push(5);
-    
-   cout << boolalpha << q2.empty() << endl;
-    cout << q3 << endl;
+int main() {
+    Queue<int> b1;
+    Queue<int> b2 = {1, 2, 3, 4};
+    Queue<int> b3{a2};
+    cout << a2 << endl;
+    cout << b3 << endl;
+    b1.swap(b3);
+    cout << b1 << endl;
+    cout << boolalpha << b3.empty() << endl;
+    cout << b1.size() << endl;
+    cout << b2.size() << endl;
+    a3.push(5);
+   cout << boolalpha << b2.empty() << endl;
+    cout << b3 << endl;
     ofstream output{ "queue.bin" };
-    output << q3;
+    output << b3;
     output.close();
-    Queue<int> q4;
+    Queue<int> b4;
     ifstream input{ "queue.bin" };
-    input >> q4;
+    input >> b4;
     input.close();
-    cout << (q4 == q3) << endl;
+    cout << (b4 == b3) << endl;
     try {
-        q4.front();
+        b4.front();
     }
-    catch (logic_error e) { cout << "empty" << endl; }
-    //cout << q5.back() << endl;
-    q4.pop();
-    cout << q4 << endl;
+    catch (logic_error e) { cout << "empty" << endl; 
+    b4.pop();
+    cout << b4 << endl;
     try {
-        q4.front() = 6;
+        b4.front() = 6;
     }
     catch (logic_error e) { cout << e.what() << endl; }
     exit(0);
